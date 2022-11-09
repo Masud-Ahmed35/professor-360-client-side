cc0dc5cd194cd4b5b50b1af0a494f709e29e344b
Masud-Ahmed35 <masud.cse.uap@gmail.com>
2022-11-09 12:45:28 +0600

    delete review

--- src/pages/MyReviews/MyReviews.js
+++ src/pages/MyReviews/MyReviews.js
@@ -1,31 +1,40 @@
 import { Table } from 'flowbite-react';
 import React, { useContext, useEffect, useState } from 'react';
+import { toast } from 'react-toastify';
 import { AuthContext } from '../../contexts/AuthProvider/AuthProvider';
 import MyReviewsRow from './MyReviewsRow';
 
 const MyReviews = () => {
     const { user } = useContext(AuthContext);
     const [myReviews, setMyReviews] = useState([]);
+    const [refresh, setRefresh] = useState(true);
 
     useEffect(() => {
         fetch(`http://localhost:7007/reviews?email=${user?.email}`)
             .then(res => res.json())
             .then(data => {
-                console.log(data.data);
                 if (data.success) {
                     setMyReviews(data.data);
                 }
             })
 
-    }, [user?.email])
+    }, [user?.email, refresh])
 
     const handleDeleteReview = id => {
         const confirmation = window.confirm('Are you sure, You want to delete this review?');
 
         if (confirmation) {
-            fetch(``, {
+            fetch(`http://localhost:7007/reviews/${id}`, {
                 method: 'DELETE'
             })
+                .then(res => res.json())
+                .then(data => {
+                    if (data.data.deletedCount) {
+                        toast.success(data.message);
+                        setRefresh(!refresh);
+                    }
+                })
+                .catch(error => toast.error(error.message));
         }
     }
 
